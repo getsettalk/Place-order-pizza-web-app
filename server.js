cc90f8ca369e97db8ed6be6ca6af7b259b584df3
sujeet kumar <rajrock7254@gnail.com>
2022-06-30 07:15:12 -0400

    basic changes

--- server.js
+++ server.js
@@ -3,13 +3,30 @@ const app = express();
 const path = require("path")
 const expressLayout = require('express-ejs-layouts');
 const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(expressLayout);
 app.set('views','views');
 app.set('view engine', 'ejs')
 
 // routes 
+// home or landing page
 app.get("/",(req,res)=>{
-    res.render("index");
+    res.render("customer/index");
+})
+
+// account page or login page
+app.get("/ac",(req,res)=>{
+    res.render("auth/ac");
+})
+
+// register page of user 
+app.get("/register",(req,res)=>{
+    res.render("auth/register");
+})
+
+// cart page 
+app.get("/cart",(req,res)=>{
+    res.render("customer/cart");
 })
 
 app.listen(port,()=>{
